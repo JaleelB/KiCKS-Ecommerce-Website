e1e7f0b552bbab17ef24da6def724cd5d88fa538
Jaleel Bennett <>
2022-09-05 19:56:04 -0400

    implemented no sign in checkout for customers

--- frontend/pages/api/stripe.js
+++ frontend/pages/api/stripe.js
@@ -6,55 +6,111 @@ export default async function handler(request, response){
 
     const session = getSession(request, response);
     const user = session?.user;
-    const stripeID = user['http://localhost:3000/stripe_customer_id'];
+    if(user){
+        const stripeID = user['http://localhost:3000/stripe_customer_id'];
 
-    if(request.method === "POST"){
-        try{
-            //create checkput session
-            const session = await stripe.checkout.sessions.create({
-                submit_type: 'pay',
-                mode: 'payment',
-                customer: stripeID,
-                payment_method_types: ['card', 'klarna'],
-                shipping_address_collection: {
-                    
-                    allowed_countries: ['US', 'CA']
-                },
-                allow_promotion_codes: true,
-
-                shipping_options: [
-                    {shipping_rate: 'shr_1LVo7fCBJx4bJphGaufId4iw'},
-                    {shipping_rate: 'shr_1LVoC1CBJx4bJphG2lP2sKB5'}
-                ],
-                line_items: request.body.map(product => {
-                    
-                    return {
-                        price_data: {
-                            currency: 'usd',
-                            product_data: {
-                                name: product.title,
-                                images: [product.image]
+        //handles checkout if a customer is signed in
+        if(request.method === "POST"){
+            try{
+                //create checkput session
+                const session = await stripe.checkout.sessions.create({
+                    submit_type: 'pay',
+                    mode: 'payment',
+                    customer: stripeID,
+                    payment_method_types: ['card', 'klarna'],
+                    shipping_address_collection: {
+                        
+                        allowed_countries: ['US', 'CA']
+                    },
+                    allow_promotion_codes: true,
+    
+                    shipping_options: [
+                        {shipping_rate: 'shr_1LVo7fCBJx4bJphGaufId4iw'},
+                        {shipping_rate: 'shr_1LVoC1CBJx4bJphG2lP2sKB5'}
+                    ],
+                    line_items: request.body.map(product => {
+                        
+                        return {
+                            price_data: {
+                                currency: 'usd',
+                                product_data: {
+                                    name: product.title,
+                                    images: [product.image]
+                                },
+                                unit_amount: product.price * 100,
+                            }, 
+                            adjustable_quantity: {
+                                enabled: true,
+                                minimum: 1,
+                                maximum: 6
                             },
-                            unit_amount: product.price * 100,
-                        }, 
-                        adjustable_quantity: {
-                            enabled: true,
-                            minimum: 1,
-                            maximum: 6
-                        },
-                        quantity: product.quantity
-                    }
-                }),
-
-                //redirect people to success/fail page
-                success_url: `${request.headers.origin}/success?&session_id={CHECKOUT_SESSION_ID}`,
-                cancel_url: `${request.headers.origin}/canceled`,
-            })
-
-            //returns stripe session on successful payment
-            response.status(200).json(session)
-        }catch(error){
-            response.status(error.statusCode || 500).json(error.message)
+                            quantity: product.quantity
+                        }
+                    }),
+    
+                    //redirect people to success/fail page
+                    success_url: `${request.headers.origin}/success?&session_id={CHECKOUT_SESSION_ID}`,
+                    cancel_url: `${request.headers.origin}/canceled`,
+                })
+    
+                //returns stripe session on successful payment
+                response.status(200).json(session)
+            }catch(error){
+                response.status(error.statusCode || 500).json(error.message)
+            }
+        }
+    }else{
+        //handles checkout if a customer isn't signed in
+        if(request.method === "POST"){
+            try{
+                //create checkput session
+                const session = await stripe.checkout.sessions.create({
+                    submit_type: 'pay',
+                    mode: 'payment',
+                    payment_method_types: ['card', 'klarna'],
+                    shipping_address_collection: {
+                        
+                        allowed_countries: ['US', 'CA']
+                    },
+                    allow_promotion_codes: true,
+    
+                    shipping_options: [
+                        {shipping_rate: 'shr_1LVo7fCBJx4bJphGaufId4iw'},
+                        {shipping_rate: 'shr_1LVoC1CBJx4bJphG2lP2sKB5'}
+                    ],
+                    line_items: request.body.map(product => {
+                        
+                        return {
+                            price_data: {
+                                currency: 'usd',
+                                product_data: {
+                                    name: product.title,
+                                    images: [product.image]
+                                },
+                                unit_amount: product.price * 100,
+                            }, 
+                            adjustable_quantity: {
+                                enabled: true,
+                                minimum: 1,
+                                maximum: 6
+                            },
+                            quantity: product.quantity
+                        }
+                    }),
+    
+                    //redirect people to success/fail page
+                    success_url: `${request.headers.origin}/success?&session_id={CHECKOUT_SESSION_ID}`,
+                    cancel_url: `${request.headers.origin}/canceled`,
+                })
+    
+                //returns stripe session on successful payment
+                response.status(200).json(session)
+            }catch(error){
+                response.status(error.statusCode || 500).json(error.message)
+            }
         }
     }
+    
+
+    
 }
